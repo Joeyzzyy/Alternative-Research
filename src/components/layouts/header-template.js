d3ea6d3da1ea3683d5cee633f03c11672c9fcab1
Yue Zhu <zhuyuejoey@gmail.com>
2025-05-14 21:10:50 +0800

    features added

--- src/components/layouts/header-template.js
+++ src/components/layouts/header-template.js
@@ -567,7 +567,7 @@ export default function Header() {
                               <svg className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
                               </svg>
-                              Manage Brand Assets
+                              Manage Brand Colors
                             </button>
                           </div>
                           {/* --- 结束：品牌颜色资产部分 --- */}
