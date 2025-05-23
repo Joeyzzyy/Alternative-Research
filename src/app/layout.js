66d326d14b49f348cf47f0acf37cb0213c809d84
Yue Zhu <zhuyuejoey@gmail.com>
2025-05-23 17:30:43 +0800

    features added

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,16 +26,23 @@ export default async function RootLayout({ children, keywords }) {
           href={faviconUrl} 
           type="image/x-icon"
         />
-        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5THNMDPJ');`}} />
-                </head>
-                <body suppressHydrationWarning={true} className="w-full min-w-full overflow-x-hidden">
-                  <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5THNMDPJ"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
-        <UserProvider>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5THNMDPJ');
+          `}
+        </Script>
+      </head>
+      <body suppressHydrationWarning={true} className="w-full min-w-full overflow-x-hidden">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5THNMDPJ"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+      <UserProvider>
           <ToolProvider>
             {children}
           </ToolProvider>
